ee7192563161a30d031406bf009a12918d2b024e
Nancy Mehra <mehranancy975@gmail.com>
2025-03-06 11:12:49 +0530

    [Nancy]Add. UC-7 added

--- main.js
+++ main.js
@@ -1,55 +1,60 @@
+
 const WAGE_PER_HOUR = 20;
 const FULL_TIME_HOURS = 8;
 const PART_TIME_HOURS = 4;
-const WORKING_DAYS_PER_MONTH = 20;
-
 const MAX_WORKING_DAYS = 20;
 const MAX_WORKING_HOURS = 160;
+const FULL_TIME_WAGE = FULL_TIME_HOURS * WAGE_PER_HOUR;
+
+const getWorkHours = (empCheck) => {
+    return empCheck === 1 ? PART_TIME_HOURS : empCheck === 2 ? FULL_TIME_HOURS : 0;
+};
 
 
-function getWorkHours(workType) {
-    switch (workType) {
-        case 1:
-            console.log("Employee worked Part Time");
-            return PART_TIME_HOURS;
-        case 2:
-            console.log("Employee worked Full Time");
-            return FULL_TIME_HOURS;
-        default:
-            console.log("Employee did not work");
-            return 0;
-    }
+let empDailyWageArray = [];
+let empDailyWageMap = new Map();
+let empDailyHoursMap = new Map();
+
+let totalEmpHours = 0;
+let totalWorkingDays = 0;
+
+
+while (totalEmpHours <= 160 && totalWorkingDays < MAX_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 3);
+    let empHours = getWorkHours(empCheck);
+    totalEmpHours += empHours;
+    let dailyWage = empHours * WAGE_PER_HOUR;
+
+    empDailyWageArray.push(dailyWage);
+    empDailyWageMap.set(totalWorkingDays, dailyWage);
+    empDailyHoursMap.set(totalWorkingDays, empHours);
 }
 
 
-let totalWage = 0;
+const totalWage = empDailyWageArray.reduce((acc, wage) => acc + wage, 0);
+console.log("Total Employee Wage:", totalWage);
 
-for (let day = 1; day <= WORKING_DAYS_PER_MONTH; day++) {
-    let workType = Math.floor(Math.random() * 3);
-    let workHours = getWorkHours(workType);
-    let dailyWage = workHours * WAGE_PER_HOUR;
-    totalWage += dailyWage;
-    console.log(`Day ${day}: Daily Wage = $${dailyWage}`);
-}
 
-let totalHours = 0;
-let totalDays = 0;
+const dailyWageWithDay = [...empDailyWageMap.entries()].map(([day, wage]) => `Day ${day}: $${wage}`);
+console.log("Daily Wage with Days:", dailyWageWithDay);
 
-while (totalDays < MAX_WORKING_DAYS && totalHours < MAX_WORKING_HOURS) {
-    let workType = Math.floor(Math.random() * 3); 
-    let workHours = getWorkHours(workType);
-    
-    if (totalHours + workHours > MAX_WORKING_HOURS) {
-        workHours = MAX_WORKING_HOURS - totalHours; 
-    }
 
-    let dailyWage = workHours * WAGE_PER_HOUR;
-    totalWage += dailyWage;
-    totalHours += workHours;
-    totalDays++;
+const fullTimeDays = [...empDailyWageMap.entries()]
+    .filter(([_, wage]) => wage === FULL_DAY_WAGE)
+    .map(([day, _]) => `Day ${day}`);
+console.log("Full Time Wage Days:", fullTimeDays);
 
-    console.log(`Day ${totalDays}: Worked ${workHours} hours, Daily Wage = $${dailyWage}`);
-}
+const firstFullTimeDay = [...empDailyWageMap.entries()].find(([_, wage]) => wage === FULL_DAY_WAGE);
+console.log("First Full Time Wage Day:", firstFullTimeDay ? `Day ${firstFullTimeDay[0]}` : "None");
 
-console.log(`Total Days Worked: ${totalDays}, Total Hours Worked: ${totalHours}`);
-console.log(`Total Monthly Wage: $${totalWage}`);
+
+const isAllFullTimeWage = empDailyWageArray.every(wage => wage === FULL_DAY_WAGE);
+console.log("All Full Time Wages?:", isAllFullTimeWage);
+
+
+const hasPartTimeWage = empDailyWageArray.some(wage => wage === PART_TIME_HOURS * WAGE_PER_HOUR);
+console.log("Has Part Time Wage?:", hasPartTimeWage);
+
+const numOfWorkedDays = [...empDailyHoursMap.values()].filter(hours => hours > 0).length;
+console.log("Total Worked Days:", numOfWorkedDays);
