967b890afc704f5690022eab99c235ce1fa1d7f0
Nancy Mehra <mehranancy975@gmail.com>
2025-03-06 11:05:56 +0530

    [Nancy]Add. UC-6added

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 const WAGE_PER_HOUR = 20;
 const FULL_TIME_HOURS = 8;
 const PART_TIME_HOURS = 4;
-<<<<<<< HEAD
-const WORKING_DAYS_PER_MONTH = 20;
-
-=======
 const MAX_WORKING_DAYS = 20;
 const MAX_WORKING_HOURS = 160;
 
 
->>>>>>> UC-5
 function getWorkHours(workType) {
     switch (workType) {
         case 1:
@@ -26,19 +21,9 @@ function getWorkHours(workType) {
 
 
 let totalWage = 0;
-<<<<<<< HEAD
-
-for (let day = 1; day <= WORKING_DAYS_PER_MONTH; day++) {
-    let workType = Math.floor(Math.random() * 3);
-    let workHours = getWorkHours(workType);
-    let dailyWage = workHours * WAGE_PER_HOUR;
-    totalWage += dailyWage;
-    console.log(`Day ${day}: Daily Wage = $${dailyWage}`);
-}
-
-=======
 let totalHours = 0;
 let totalDays = 0;
+let dailyWages = []; 
 
 while (totalDays < MAX_WORKING_DAYS && totalHours < MAX_WORKING_HOURS) {
     let workType = Math.floor(Math.random() * 3); 
@@ -53,9 +38,10 @@ while (totalDays < MAX_WORKING_DAYS && totalHours < MAX_WORKING_HOURS) {
     totalHours += workHours;
     totalDays++;
 
+    dailyWages.push(dailyWage); 
     console.log(`Day ${totalDays}: Worked ${workHours} hours, Daily Wage = $${dailyWage}`);
 }
 
 console.log(`Total Days Worked: ${totalDays}, Total Hours Worked: ${totalHours}`);
->>>>>>> UC-5
 console.log(`Total Monthly Wage: $${totalWage}`);
+console.log("Daily Wages: ", dailyWages);
